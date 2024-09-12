7b206c31e28151af02f5aadb9319e8c62f01ac1c
곽유나 <yoona405@konkuk.ac.kr>
2024-09-12 11:32:45 +0900

    build : 단위테스트를 위한 Mokito 설정 추가

---
+++ aiku/aiku-main/src/test/java/aiku_main/service/TeamServiceTest.java
@@ -0,0 +1,13 @@
+package aiku_main.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TeamServiceTest {
+
+    @Test
+    void addTeam() {
+        
+    }
+}

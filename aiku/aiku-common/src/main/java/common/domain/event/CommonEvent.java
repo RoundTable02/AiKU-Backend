72e1df2e529067724f261c689e091b94d34df351
Choi wontak <76509639+RoundTable02@users.noreply.github.com>
2024-09-13 07:42:23 +0900

    feat : 이벤트 엔티티 추가
    새로운 이벤트 추가 대비 부모 객체를 도입해  조인 전략으로 접근,
    event notice와 다대일로 구성하였습니다.

---
+++ aiku/aiku-common/src/main/java/common/domain/event/CommonEvent.java
@@ -0,0 +1,27 @@
+package common.domain.event;
+
+import common.domain.BaseTime;
+import common.domain.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "DTYPE")
+public abstract class CommonEvent extends BaseTime {
+    @Column(name = "eventId")
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(name = "memberId")
+    @OneToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @JoinColumn(name = "eventNoticeId")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private EventNotice eventNotice;
+}

63b03661b42a4e783f44cafaf7236b2c8e8b1dd3
곽유나 <yoona405@konkuk.ac.kr>
2024-09-12 11:43:58 +0900

    test : 그룹 등록 서비스 단위 테스트 작성중

--- aiku/aiku-main/src/test/java/aiku_main/service/TeamServiceTest.java
+++ aiku/aiku-main/src/test/java/aiku_main/service/TeamServiceTest.java
@@ -1,13 +1,33 @@
 package aiku_main.service;
 
+import aiku_main.repository.TeamRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class TeamServiceTest {
 
+    @InjectMocks
+    TeamService teamService;
+
+    @Mock
+    TeamRepository teamRepository;
+
     @Test
+    @DisplayName("그룹 등록")
     void addTeam() {
-        
+        //given
+
+
+        //when
+        teamService.addTeam()
+        //then
+
     }
 }

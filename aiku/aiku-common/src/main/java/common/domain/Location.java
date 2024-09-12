9e713ad5a3c9f9e257b750bf8353db7fd1ba354c
곽유나 <yoona405@konkuk.ac.kr>
2024-09-12 17:09:13 +0900

    test : 스케줄 수정 단위 테스트 작성

--- aiku/aiku-common/src/main/java/common/domain/Location.java
+++ aiku/aiku-common/src/main/java/common/domain/Location.java
@@ -3,10 +3,12 @@ package common.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor
 @Embeddable
 public class Location {

c9d4f11a1b304c5e79d32bee86303cfcf536470a
곽유나 <yoona405@konkuk.ac.kr>
2024-09-12 13:52:12 +0900

    ScheduleRepository 추가

---
+++ aiku/aiku-main/src/main/java/aiku_main/repository/ScheduleRepository.java
@@ -0,0 +1,7 @@
+package aiku_main.repository;
+
+import common.domain.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+}

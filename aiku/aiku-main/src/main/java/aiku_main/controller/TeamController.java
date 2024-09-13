d9a8a2d00ba1a909eb85a82b6e21f044464172e1
곽유나 <yoona405@konkuk.ac.kr>
2024-09-14 07:13:02 +0900

    feat : 그룹 입장, 퇴장 controller 구현

--- aiku/aiku-main/src/main/java/aiku_main/controller/TeamController.java
+++ aiku/aiku-main/src/main/java/aiku_main/controller/TeamController.java
@@ -5,10 +5,7 @@ import aiku_main.service.TeamService;
 import common.response.BaseResponse;
 import common.response.status.BaseCode;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/groups")
 @RequiredArgsConstructor
@@ -24,5 +21,18 @@ public class TeamController {
         return BaseResponse.getSimpleRes(teamId, BaseCode.POST);
     }
 
+    @PostMapping("/{groupId}/enter")
+    public BaseResponse enterTeam(@RequestParam Long groupId){
+        Long teamId = teamService.enterTeam(null, groupId);
+
+        return BaseResponse.getSimpleRes(teamId, BaseCode.ENTER);
+    }
+
+    @PostMapping("/{groupId}/exit")
+    public BaseResponse exitTeam(@RequestParam Long groupId){
+        Long teamId = teamService.exitTeam(null, groupId);
+
+        return BaseResponse.getSimpleRes(teamId, BaseCode.EXIT);
+    }
 
 }

e2aa7c2fc7a0505e4bd322122582009c5f225500
곽유나 <yoona405@konkuk.ac.kr>
2024-09-14 17:54:44 +0900

    fix : Member도메인 변화로 인한 오류 수정

--- aiku/aiku-main/src/main/java/aiku_main/dto/MemberProfileResDto.java
+++ aiku/aiku-main/src/main/java/aiku_main/dto/MemberProfileResDto.java
@@ -1,16 +1,19 @@
 package aiku_main.dto;
 
-import MemberProfile;
+import common.domain.member.MemberProfile;
+import common.domain.member.MemberProfileBackground;
+import common.domain.member.MemberProfileCharacter;
+import common.domain.member.MemberProfileType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class MemberProfileResDto{
-    private String profileType;
+    private MemberProfileType profileType;
     private String profileImg;
-    private String profileCharacter;
-    private String profileBackground;
+    private MemberProfileCharacter profileCharacter;
+    private MemberProfileBackground profileBackground;
 
     public MemberProfileResDto(MemberProfile profile) {
         this.profileType = profile.getProfileType();

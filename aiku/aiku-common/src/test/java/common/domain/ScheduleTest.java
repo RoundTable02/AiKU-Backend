4ebdb70d7256f53f7e3eac704b3d510eaa7fc14f
곽유나 <yoona405@konkuk.ac.kr>
2024-09-15 18:37:09 +0900

    test : Schedule 엔티티 멤버 제거 단위 테스트

--- aiku/aiku-common/src/test/java/common/domain/ScheduleTest.java
+++ aiku/aiku-common/src/test/java/common/domain/ScheduleTest.java
@@ -2,12 +2,17 @@ package common.domain;
 
 import common.domain.member.Member;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Random;
 
+import static common.domain.Status.ALIVE;
+import static common.domain.Status.DELETE;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
 
 class ScheduleTest {
 
@@ -96,6 +101,35 @@ class ScheduleTest {
         assertThat(scheduleMembers).extracting("pointAmount").contains(0, 100);
         assertThat(scheduleMembers.stream().map(ScheduleMember::getMember).map(Member::getNickname))
                 .contains(member.getNickname(), member2.getNickname());
+    }
+
+    @Test
+    void removeScheduleMember(){
+        //given
+        Member member = spy(Member.create("member"));
+        Member member2 = spy(Member.create("member2"));
+        Long memberId1 = getRandomId();
+        Long memberId2 = getRandomId();
+
+        Long teamId = getRandomId();
+
+        Schedule schedule = Schedule.create(member, teamId, "sch1", LocalDateTime.now(),
+                new Location("loc1", 1.0, 1.0), 0);
+        int point = 1000;
+        schedule.addScheduleMember(member2, false, point);
+
+        doReturn(memberId1).when(member).getId();
+        doReturn(memberId2).when(member2).getId();
+
+        //when
+        int returnPoint = schedule.removeScheduleMember(member2);
+
+        //then
+        assertThat(returnPoint).isEqualTo(point);
+
+        List<ScheduleMember> scheduleMembers = schedule.getScheduleMembers();
+        assertThat(scheduleMembers.size()).isEqualTo(2);
+        assertThat(scheduleMembers).extracting("status").contains(ALIVE, DELETE);
     }
 
     Long getRandomId(){

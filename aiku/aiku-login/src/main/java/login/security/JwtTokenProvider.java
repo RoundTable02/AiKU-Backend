a4e272fc0359181dac149df5ad0081ce22b4387a
Choi wontak <76509639+RoundTable02@users.noreply.github.com>
2024-09-29 08:33:31 +0900

    feat : Refresh Token 추가

--- aiku/aiku-login/src/main/java/login/security/JwtTokenProvider.java
+++ aiku/aiku-login/src/main/java/login/security/JwtTokenProvider.java
@@ -32,21 +32,47 @@ public class JwtTokenProvider {
     }
 
     // 유저 정보 -> Token 생성
-    public String generateToken(Authentication authentication) {
+    public JwtToken generateToken(Authentication authentication) {
+        if (authentication == null)
+            throw new JwtAccessDeniedException("자격 증명에 실패하였습니다.");
+
         String authorities = authentication.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.joining(","));
 
+        String accessToken = generateAccessToken(authentication, authorities);
+        String refreshToken = generateRefreshToken();
+
+        return JwtToken.builder()
+                .grantType("Bearer")
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+
+    public String generateAccessToken(Authentication authentication, String authorities) {
         Calendar accessTokenCal = Calendar.getInstance();
         accessTokenCal.setTime(new Date());
         accessTokenCal.add(Calendar.DATE, 1);
-
         // AccessToken : 1일 후 만료
         Date accessTokenExpiresIn = accessTokenCal.getTime();
         return Jwts.builder()
-                .setSubject(authentication.getName())
+                .setSubject(authentication.getName()) // email
                 .claim("auth", authorities)
                 .setExpiration(accessTokenExpiresIn)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String generateRefreshToken() {
+        Calendar refreshTokenCal = Calendar.getInstance();
+        refreshTokenCal.setTime(new Date());
+        refreshTokenCal.add(Calendar.MONTH, 1);
+
+        // RefreshToken : 1달 후 만료
+        Date refreshTokenExpiresIn = refreshTokenCal.getTime();
+        return Jwts.builder()
+                .setExpiration(refreshTokenExpiresIn)
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
@@ -88,7 +114,6 @@ public class JwtTokenProvider {
             log.info("JWT claims string is empty.", e);
             throw new JwtAccessDeniedException("정상적이지 않은 접근입니다.");
         }
-//        return false;
     }
 
     // 넘어온 accessToken의 정보 조회

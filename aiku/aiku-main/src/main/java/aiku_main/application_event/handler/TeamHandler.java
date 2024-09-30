89d5d7c76607c732d02891098168c3bbe4e1b9dd
곽유나 <yoona405@konkuk.ac.kr>
2024-09-30 23:43:41 +0900

    refactor : event handler 메서드명 변경

--- aiku/aiku-main/src/main/java/aiku_main/application_event/handler/TeamHandler.java
+++ aiku/aiku-main/src/main/java/aiku_main/application_event/handler/TeamHandler.java
@@ -2,6 +2,7 @@ package aiku_main.application_event.handler;
 
 import aiku_main.application_event.event.ScheduleAutoCloseEvent;
 import aiku_main.application_event.event.TeamExitEvent;
+import aiku_main.service.ScheduleService;
 import aiku_main.service.TeamService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
@@ -14,15 +15,18 @@ import org.springframework.transaction.event.TransactionalEventListener;
 public class TeamHandler {
 
     private final TeamService teamService;
+    private final ScheduleService scheduleService;
 
     @EventListener
     public void analyzeLateTimeResult(ScheduleAutoCloseEvent event){
-        teamService.analyzeLateTimeResult(event.getSchedule().getId());
+        if(scheduleService.isScheduleAutoClosed(event.getSchedule().getId())){
+            teamService.analyzeLateTimeResult(event.getSchedule().getId());
+        }
     }
 
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void handleTeamExitEvent(TeamExitEvent event) {
+    public void updateLateTimeResultOfExitMember(TeamExitEvent event) {
         teamService.updateLateTimeResultOfExitMember(event.getMember().getId(), event.getTeam().getId());
     }
 }

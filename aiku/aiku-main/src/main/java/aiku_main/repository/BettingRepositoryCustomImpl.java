1448c46bacf1de39ec073ffc0e992674ad01ce10
kyoona <yoona405@konkuk.ac.kr>
2024-10-09 19:11:58 +0900

    refactor : 필요없는 주석 제거

--- aiku/aiku-main/src/main/java/aiku_main/repository/BettingRepositoryCustomImpl.java
+++ aiku/aiku-main/src/main/java/aiku_main/repository/BettingRepositoryCustomImpl.java
@@ -1,26 +1,16 @@
 package aiku_main.repository;
 
-import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import common.domain.Betting;
 import common.domain.ExecStatus;
-import common.domain.member.QMember;
-import common.domain.team.QTeam;
-import common.domain.team.QTeamMember;
 import common.domain.value_reference.ScheduleMemberValue;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
-import static common.domain.ExecStatus.TERM;
 import static common.domain.QBetting.betting;
 import static common.domain.Status.ALIVE;
-import static common.domain.member.QMember.member;
 import static common.domain.schedule.QScheduleMember.scheduleMember;
-import static common.domain.team.QTeam.team;
-import static common.domain.team.QTeamMember.teamMember;
 
 @RequiredArgsConstructor
 public class BettingRepositoryCustomImpl implements BettingRepositoryCustom{

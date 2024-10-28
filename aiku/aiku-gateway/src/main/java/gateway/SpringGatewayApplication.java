0a64ba8dae0ae2567127271f307fea86dbef6f43
kyoona <yoona405@konkuk.ac.kr>
2024-10-29 00:22:14 +0900

    feat : SpringGateway에 대한 공통 빌드 분리

--- aiku/aiku-gateway/src/main/java/gateway/SpringGatewayApplication.java
+++ aiku/aiku-gateway/src/main/java/gateway/SpringGatewayApplication.java
@@ -1,22 +1,14 @@
 package gateway;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class SpringGatewayApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(SpringGatewayApplication.class);
-    }
-
-
-    @Bean
-    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
-        return new JPAQueryFactory(em);
     }
 }

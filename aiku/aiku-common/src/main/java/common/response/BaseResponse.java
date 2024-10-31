9a80312c29b31d24ce402a5e122771e372ffffa9
kyoona <yoona405@konkuk.ac.kr>
2024-10-31 13:46:26 +0900

    feat : 응답에 request_id 적용

--- aiku/aiku-common/src/main/java/common/response/BaseResponse.java
+++ aiku/aiku-common/src/main/java/common/response/BaseResponse.java
@@ -3,6 +3,7 @@ package common.response;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.slf4j.MDC;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PACKAGE)
@@ -10,12 +11,12 @@ public class BaseResponse<T> {
     private String requestId;
     private T result;
 
-    //TODO 후에 MDC 스레드 UUID를 통해 requestId 변경
     public BaseResponse(T result) {
+        this.requestId = MDC.get("request_id");
         this.result = result;
     }
 
     public static BaseResponse<BaseResultDto> getSimpleRes(Long id) {
-        return new BaseResponse<>(null, BaseResultDto.get(id));
+        return new BaseResponse<>(MDC.get("request_id"), BaseResultDto.get(id));
     }
 }

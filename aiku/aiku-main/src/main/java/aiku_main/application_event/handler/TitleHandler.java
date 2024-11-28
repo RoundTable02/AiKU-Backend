a41d87d9f46669ff9385fef75b949a058f74dac2
Choi wontak <76509639+RoundTable02@users.noreply.github.com>
2024-11-28 23:13:15 +0900

    fix : AutoClose 이벤트 의존 수정

--- aiku/aiku-main/src/main/java/aiku_main/application_event/handler/TitleHandler.java
+++ aiku/aiku-main/src/main/java/aiku_main/application_event/handler/TitleHandler.java
@@ -1,11 +1,8 @@
 package aiku_main.application_event.handler;
 
-import aiku_main.application_event.event.ScheduleAutoCloseEvent;
 import aiku_main.application_event.event.ScheduleCloseEvent;
-import aiku_main.service.ScheduleService;
 import aiku_main.service.TitleService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
@@ -18,15 +15,6 @@ import java.util.List;
 public class TitleHandler {
 
     private final TitleService titleService;
-    private final ScheduleService scheduleService;
-
-    @Async
-    @EventListener
-    public void checkAllAvailableMemberTitleInSchedule(ScheduleAutoCloseEvent event){
-        if (scheduleService.isScheduleAutoClosed(event.getSchedule().getId())) {
-            checkAllAvailableMemberTitleInSchedule(event.getSchedule().getId());
-        }
-    }
 
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)

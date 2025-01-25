1b3cfcc6570e28df78696c61104aa7d2ad5e6baa
kyoona <yoona405@konkuk.ac.kr>
2025-01-25 14:34:26 +0900

    refactor : application event에서 엔티티에 사용되는 값 객체 제거

--- aiku/aiku-main/src/main/java/aiku_main/application_event/event/ScheduleOpenEvent.java
+++ aiku/aiku-main/src/main/java/aiku_main/application_event/event/ScheduleOpenEvent.java
@@ -1,16 +1,11 @@
 package aiku_main.application_event.event;
 
-import common.domain.schedule.Schedule;
-import common.domain.value_reference.ScheduleValue;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class ScheduleOpenEvent {
 
-    private ScheduleValue schedule;
-
-    public ScheduleOpenEvent(Schedule schedule) {
-        this.schedule = new ScheduleValue(schedule);
-    }
+    private Long scheduleId;
 }

6bf6fc3b5a8cd401ed4ee468b003913e761cf62e
kyoona <yoona405@konkuk.ac.kr>
2025-03-02 17:20:13 +0900

    feat : 스케줄 입장 롤백 로직 개발 및 테스트

--- aiku/aiku-main/src/main/java/aiku_main/service/strategy/ScheduleEnterRollbackStrategy.java
+++ aiku/aiku-main/src/main/java/aiku_main/service/strategy/ScheduleEnterRollbackStrategy.java
@@ -2,15 +2,33 @@ package aiku_main.service.strategy;
 
 import aiku_main.application_event.event.PointChangeReason;
 import aiku_main.application_event.event.PointChangeType;
-import common.domain.value_reference.MemberValue;
-import org.springframework.stereotype.Component;
+import aiku_main.repository.ScheduleQueryRepository;
+import aiku_main.scheduler.ScheduleScheduler;
+import common.domain.schedule.Schedule;
+import common.domain.schedule.ScheduleMember;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @PointChangeReasonMapping(PointChangeReason.SCHEDULE_ENTER)
-@Component
+@RequiredArgsConstructor
+@Transactional
+@Service
 public class ScheduleEnterRollbackStrategy implements RollbackStrategy {
+
+    private final ScheduleQueryRepository scheduleRepository;
+    private final ScheduleScheduler scheduleScheduler;
 
     @Override
     public void execute(Long memberId, PointChangeType pointChangeType, int pointAmount, PointChangeReason pointChangeReason, Long reasonId) {
-        System.out.println("Processing SCHEDULE_ENTER rollback");
+        Schedule schedule = scheduleRepository.findById(reasonId).orElseThrow();
+        ScheduleMember scheduleMember = scheduleRepository.findScheduleMember(memberId, schedule.getId()).orElseThrow();
+
+        schedule.errorScheduleMember(scheduleMember);
+
+        if (scheduleMember.isOwner()) {
+            schedule.error();
+            scheduleScheduler.cancelAll(schedule.getId());
+        }
     }
 }

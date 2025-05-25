4aae4a9c3043dd0b91ded2a919852d017fc61681
Choi wontak <76509639+RoundTable02@users.noreply.github.com>
2025-05-25 21:29:54 +0900

    fix : async로 트랜잭션 분리

--- aiku/aiku-map/src/main/java/map/application_event/handler/MemberArrivalEventHandler.java
+++ aiku/aiku-map/src/main/java/map/application_event/handler/MemberArrivalEventHandler.java
@@ -5,6 +5,7 @@ import map.application_event.event.MemberArrivalEvent;
 import map.service.MapService;
 import map.service.RacingService;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -16,6 +17,7 @@ public class MemberArrivalEventHandler {
     private final RacingService racingService;
     private final MapService mapService;
 
+    @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void makeMemberWinnerInRacing(MemberArrivalEvent event){
         racingService.makeMemberWinnerInRacing(event.getMemberId(), event.getScheduleId(), event.getScheduleName());

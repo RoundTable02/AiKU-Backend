4d4723cf431364f01fb8c06938a7d58bae4f729c
Choi wontak <76509639+RoundTable02@users.noreply.github.com>
2025-05-28 01:51:49 +0900

    refactor : 트랜잭션 범위 재설정으로 모듈 내 불필요한 Saga 코드 제거

--- aiku/aiku-main/src/main/java/aiku_main/service/member/MemberPointChangeFacade.java
+++ aiku/aiku-main/src/main/java/aiku_main/service/member/MemberPointChangeFacade.java
@@ -1,13 +1,10 @@
 package aiku_main.service.member;
 
-import aiku_main.application_event.event.PointChangeFailEvent;
 import aiku_main.application_event.event.PointChangeReason;
 import aiku_main.application_event.event.PointChangeType;
-import aiku_main.exception.PointChangeFailException;
 import aiku_main.service.log.PointLogService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,22 +16,13 @@ public class MemberPointChangeFacade {
 
     private final MemberService memberService;
     private final PointLogService pointLogService;
-    private final ApplicationEventPublisher publisher;
 
     @Transactional
     public void makePointChange(Long memberId, PointChangeType pointChangeType, int pointAmount, PointChangeReason pointChangeReason, Long reasonId) {
-        try {
-            // 멤버 포인트 변화
-            memberService.updateMemberPoint(memberId, pointChangeType, pointAmount);
-            // 로그 기록
-            pointLogService.savePointLog(pointChangeReason, memberId, pointChangeType, pointAmount, reasonId);
-        } catch (Exception e) {
-            // 실패 이벤트 publish
-            PointChangeFailEvent event = new PointChangeFailEvent(memberId, pointChangeType, pointAmount, pointChangeReason, reasonId);
-            publisher.publishEvent(event);
-            
-            throw new PointChangeFailException();
-        }
+        // 멤버 포인트 변화
+        memberService.updateMemberPoint(memberId, pointChangeType, pointAmount);
+        // 로그 기록
+        pointLogService.savePointLog(pointChangeReason, memberId, pointChangeType, pointAmount, reasonId);
 
     }
 
